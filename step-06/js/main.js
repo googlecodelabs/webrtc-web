13ea1b2d0f0f9e1282aec93697113c4069c33109
Philipp Hancke <fippo@appear.in>
2017-04-11 18:15:41 +0200

    dont use non-spec constructs
    replaces RTCIceServer.url with RTCIceServer.urls and the legacy-legacy
    mandatory OfferToReceive options with the less legacy offerToReceive options

--- step-06/js/main.js
+++ step-06/js/main.js
@@ -6,10 +6,9 @@
 
 // var configuration = {
 //   'iceServers': [{
-//     'url': 'stun:stun.l.google.com:19302'
+//     'urls': 'stun:stun.l.google.com:19302'
 //   }]
 // };
-// {'url':'stun:stun.services.mozilla.com'}
 
 var configuration = null;
 
