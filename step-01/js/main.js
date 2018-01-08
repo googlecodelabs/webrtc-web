893e52b7fe527d34b4cc7edaa2db21f9aa25614c
Nito Buendia <effectedcard@gmail.com>
2018-01-08 20:39:01 +0800

    Change comment style

--- step-01/js/main.js
+++ step-01/js/main.js
@@ -1,22 +1,19 @@
 'use strict';
 
-/**
- * On this codelab, you will be streaming only video (true).
- * Constraints to apply to MediaStream.
- */
+// On this codelab, you will be streaming only video (video: true).
 const mediaStreamConstraints = {
   video: true,
 };
 
-/** Video element where stream will be placed. */
+// Video element where stream will be placed.
 const localVideo = document.querySelector('video');
 
-/** Handles success by adding the MediaStream to the video element. */
+// Handles success by adding the MediaStream to the video element.
 const gotLocalMediaStream = (mediaStream) => {
   localVideo.srcObject = mediaStream;
 };
 
-/** Handles error by logging a message to the console with the error message. */
+// Handles error by logging a message to the console with the error message.
 const localMediaStreamError = (error) => {
   console.log('navigator.getUserMedia error: ', error);
 };
