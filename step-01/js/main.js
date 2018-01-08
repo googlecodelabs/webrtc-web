086f8bdc81d96d1e4c8ca55f1877d4d3878b2c1d
Nito Buendia <effectedcard@gmail.com>
2018-01-08 20:33:56 +0800

    Simplify function names, some comments and additional text.

--- step-01/js/main.js
+++ step-01/js/main.js
@@ -1,10 +1,8 @@
 'use strict';
 
-// Define navigator.mediaDevices.getUserMedia() for older devices.
-
 /**
- * Constraints to apply to MediaStream.
  * On this codelab, you will be streaming only video (true).
+ * Constraints to apply to MediaStream.
  */
 const mediaStreamConstraints = {
   video: true,
@@ -14,17 +12,15 @@ const mediaStreamConstraints = {
 const localVideo = document.querySelector('video');
 
 /** Handles success by adding the MediaStream to the video element. */
-const localMediaStreamSuccessCallback = (mediaStream) => {
-  window.localStream = mediaStream;  // Makes stream available to the console.
+const gotLocalMediaStream = (mediaStream) => {
   video.srcObject = mediaStream;
 };
 
 /** Handles error by logging a message to the console with the error message. */
-const localMediaStreamErrorCallback = (error) => {
+const localMediaStreamError = (error) => {
   console.log('navigator.getUserMedia error: ', error);
 };
 
 // Initializes media stream.
 navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
-  .then(localMediaStreamSuccessCallback)
-  .catch(localMediaStreamErrorCallback);
+  .then(gotLocalMediaStream).catch(localMediaStreamError);
