a4703dc67d0857d52911c68ce4dc42f20629dc79
Nito Buendia <effectedcard@gmail.com>
2018-01-23 21:38:59 +0800

    Remove ; after function definitions

--- step-01/js/main.js
+++ step-01/js/main.js
@@ -11,12 +11,12 @@ const localVideo = document.querySelector('video');
 // Handles success by adding the MediaStream to the video element.
 function gotLocalMediaStream(mediaStream) {
   localVideo.srcObject = mediaStream;
-};
+}
 
 // Handles error by logging a message to the console with the error message.
 function handleLocalMediaStreamError(error) {
   console.log('navigator.getUserMedia error: ', error);
-};
+}
 
 // Initializes media stream.
 navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
